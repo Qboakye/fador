9d903d639e9c2e3c4662576997bfe5ffaac787c2
qboakye <qboakye@gmail.com>
2023-03-19 23:13:37 +0000

    checked for current env whether prod or dev and set url accordingly

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const express = require("express")
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
-const helmet = require("helmet")
-const dotenv = require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const dotenv = require("dotenv").config();
 
-const logger = require("./helpers/logger")
-const rateLimiter = require("./helpers/rateLimit")
+const logger = require("./helpers/logger");
+const rateLimiter = require("./helpers/rateLimit");
 
-const app = express()
-const router = require("./routes/routes")
+const app = express();
+const router = require("./routes/routes");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || "development";
 
-app.use(helmet())
-app.use(rateLimiter)
-app.use(cors({
+app.use(helmet());
+app.use(rateLimiter);
+const origin =
+  env === "development"
+    ? "http://localhost:5173"
+    : "https://q-emailgen.surge.sh";
+
+app.use(
+  cors({
     credentials: true,
-    origin: "http://127.0.0.1:5500"
-}))
-app.use(express.json())
-app.use(cookieParser())
+    origin,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/", router)
+app.use("/", router);
 
-app.listen(port,()=>{
-    logger.info(`Server is running on http://127.0.0.1:${port}/`) 
-})
+app.listen(port, () => {
+  logger.info(`Server is running on http://127.0.0.1:${port}/`);
+});
